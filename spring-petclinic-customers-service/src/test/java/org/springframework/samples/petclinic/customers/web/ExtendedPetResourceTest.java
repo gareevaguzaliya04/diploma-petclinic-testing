df9f949249e9253000c604ce0fcc2542c27ecff7
Гареева Гузалия <gareeva.guzaliya04@gmail.com>
2026-06-16 02:12:21 +0500

    fix: resolve ambiguous Owner import in ExtendedPetResourceTest

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/ExtendedPetResourceTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/ExtendedPetResourceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.customers.model.*;
+import org.springframework.samples.petclinic.customers.model.Owner;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
